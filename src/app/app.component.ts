3da07dc4675dd6f077d23cc0aa4391843c9a0eaf
Sergey Kryuchkov <sergeykr101@gmail.com>
2019-10-11 17:26:28 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularTestGrid';
+  rowData: any;
+  components: any;
+
+  columnDefs = [
+    {
+      headerName: 'thumbnails',
+      field: 'thumbnails',
+      cellRenderer: 'thumbnailsImageRender',
+      checkboxSelection: true,
+      headerCheckboxSelection: true,
+      headerCheckboxSelectionFilteredOnly: true,
+    },
+    {
+      headerName: 'publishedAt',
+      field: 'publishedAt',
+      sortable: true
+    },
+    {
+      headerName: 'title',
+      field: 'title',
+      sortable: true,
+      cellRenderer: 'titleLinksRenderer',
+    },
+    {
+      headerName: 'description',
+      field: 'description',
+      sortable: true
+    }
+  ];
+
+  constructor(private http: HttpClient) {
+    this.components = {
+      titleLinksRenderer: this.titleLinksRenderer,
+      thumbnailsImageRender: this.thumbnailsImageRender
+    };
+  }
+
+  titleLinksRenderer(params) {
+    if (params.value === undefined || params.value === null) {
+      return '';
+    } else {
+      const {videoId, title} = params.data;
+      return `<a href='https://www.youtube.com/watch?v=${videoId}' target="_blank">${title}</a>`;
+    }
+  }
+
+  thumbnailsImageRender(params) {
+    if (params.value === undefined || params.value === null) {
+      return '';
+    } else {
+      const {thumbnails} = params.data;
+      return `<img src='${thumbnails}'>`;
+    }
+  }
+
+  ngOnInit() {
+    const data = this.http.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk&maxResults=' +
+      '50&type=video&part=snippet&q=john');
+
+    data.subscribe(res => {
+      this.rowData = res.items.map(item => {
+        const {title, publishedAt, description, thumbnails} = item.snippet;
+        return {
+          videoId: item.id.videoId,
+          title,
+          thumbnails: thumbnails.default.url,
+          publishedAt,
+          description,
+        };
+      });
+    });
+  }
 }
